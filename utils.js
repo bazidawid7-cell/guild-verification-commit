8d2cd2d887701a85b7b466b258dd3a1ceac04d18
bazidawid7-cell <bazidawid7@gmail.com>
2026-03-25 21:14:13 +0100

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:13:56.085Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-25T20:14:10.692Z
+Commit: Patch security vulnerability in authentication
