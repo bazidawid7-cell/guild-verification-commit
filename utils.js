6cb319e784d1cf79c822540b1ffb89a2c732a5a8
bazidawid7-cell <bazidawid7@gmail.com>
2026-03-25 21:13:09 +0100

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:13:01.981Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-25T20:13:07.196Z
+Commit: Add multi-language support
