f9812a23e87e41dd2e703717e76e4407b3ff03de
bazidawid7-cell <bazidawid7@gmail.com>
2026-03-25 21:12:44 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:12:27.803Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-25T20:12:42.406Z
+Commit: Fix CSS styling bug on mobile devices
