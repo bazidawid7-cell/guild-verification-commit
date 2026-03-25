dd602fdcbeb8dd55e77095880a7f2a15de95c617
bazidawid7-cell <bazidawid7@gmail.com>
2026-03-25 21:13:31 +0100

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:13:10.096Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-25T20:13:29.111Z
+Commit: Resolve null pointer exception in user service
