35dd4a9f11d4def52eba8ecdf83298c1ac013170
bazidawid7-cell <bazidawid7@gmail.com>
2026-03-25 21:12:52 +0100

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:12:44.849Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-25T20:12:50.501Z
+Commit: Fix broken links in documentation
