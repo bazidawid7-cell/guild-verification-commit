1568240d915f12b4bdd9a056adff06e7dd620f0e
bazidawid7-cell <bazidawid7@gmail.com>
2026-03-25 21:13:07 +0100

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:12:50.501Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-25T20:13:04.742Z
+Commit: Fix memory leak in event handlers
