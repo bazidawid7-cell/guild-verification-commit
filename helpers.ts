a891d7554cda283d48b567bc8a828148125bd355
bazidawid7-cell <bazidawid7@gmail.com>
2026-03-25 21:13:35 +0100

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:13:27.911Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-25T20:13:32.807Z
+Commit: Fix memory leak in event handlers
