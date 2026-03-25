96b183bacf2a525d1041a8f78c7ed5ecd833af82
bazidawid7-cell <bazidawid7@gmail.com>
2026-03-25 21:14:01 +0100

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T20:13:54.827Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-25T20:13:58.683Z
+Commit: Fix CSS styling bug on mobile devices
